5aea77b45399834d8245c81b07b21606279e37fc
dhanushkasampath <dhanushka.2018410@iit.ac.lk>
2024-10-18 10:59:07 +0800

    Bcrypted the password when saving

--- src/main/java/com/learn/spring_security_demo/service/UserService.java
+++ src/main/java/com/learn/spring_security_demo/service/UserService.java
@@ -3,6 +3,7 @@ package com.learn.spring_security_demo.service;
 import com.learn.spring_security_demo.entity.Users;
 import com.learn.spring_security_demo.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,7 +12,10 @@ public class UserService {
 
     private final UserRepository userRepository;
 
+    private final BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(12);
+
     public Users register(Users users){
+        users.setPassword(bCryptPasswordEncoder.encode(users.getPassword()));
         return userRepository.save(users);
     }
 }
